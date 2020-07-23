9710c1b0b3abbef56beabc0a4adb3b6c16bc8e57
MohFig <abdeloifimohamed@gmail.com>
2020-07-23 14:46:31 +0200

    some files added to my html project

---
+++ exercices PHP/inclusion.php
@@ -0,0 +1,16 @@
+Pour faire une inclusion en php on place la commande entre les balises php au milieu de a page html
+
+
+<?php
+
+
+
+
+
+<?php include ("entete.php") ?>
+
+
+
+
+
+?>
---
+++ exercices PHP/test1.php
@@ -0,0 +1,5 @@
+<?php
+    $ moh = "Mohamed";
+    echo($moh);
+
+    
